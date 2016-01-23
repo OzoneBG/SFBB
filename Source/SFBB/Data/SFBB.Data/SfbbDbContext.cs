3d44e82862fc2d5aac428471e5240795aa601820
OzoneBG <muzunov875@gmail.com>
2016-01-23 15:21:30 +0200

    Restructured some of the classes

---
+++ Source/SFBB/Data/SFBB.Data/SfbbDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace SFBB.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using SFBB.Data.Models;
+
+    public class SfbbDbContext : IdentityDbContext<User>
+    {
+        public SfbbDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SfbbDbContext Create()
+        {
+            return new SfbbDbContext();
+        }
+    }
+}
