81deea329afc2f787b591477b8575209ade9a2eb
OzoneBG <muzunov875@gmail.com>
2016-01-24 22:42:56 +0200

    Integrated AutoMapper for SFBB.Web and infrastructure

---
+++ Source/SFBB/Web/SFBB.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace SFBB.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
---
+++ Source/SFBB/Web/SFBB.Web.Infrastructure/Mapping/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace SFBB.Web.Infrastructure.Mapping
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
